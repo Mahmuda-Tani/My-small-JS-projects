0a742fb92fa98beeb62ac317cac0082375fe0061
Mahmuda-Tani <79637311+Mahmuda-Tani@users.noreply.github.com>
2021-07-31 23:08:33 +0600

    Add files via upload

---
+++ counterAndcolour_game/script.js
@@ -0,0 +1,75 @@
+// var counter= document.querySelector(".counter");
+// var followers= document.querySelector(".followers");
+
+
+// let count=1;
+// setInterval(()=>{
+//     if(count<1000)
+//     {
+//         count++;
+//         counter.innerText=count;
+//     }
+// },1)
+
+
+// setTimeout(()=>{
+//     followers.innerText="bcZ I <3 U";
+// },4400)
+
+
+
+
+// const red = document.querySelector(".counter");
+// const blue=document.querySelector(".blue");
+
+// console.log(red);
+
+// console.log(window.getComputedStyle(red).color);
+
+// function abc(x)
+// {
+//     return window.getComputedStyle(red).color;
+// }
+
+// var xx=abc(red);
+// console.log(xx);
+
+
+
+
+
+const red=document.querySelector(".red");
+const  blue=document.querySelector(".blue");
+const yellow=document.querySelector(".yellow");
+const  cyan=document.querySelector(".cyan");
+const center=document.querySelector(".center");
+
+// var x=window.getComputedStyle(yellow).backgroundColor;
+
+// red.addEventListener("mouseenter",()=>{
+//     center.style.background = x;
+// });
+
+/// jhakanaka part
+
+// function getcolor(x)
+// {
+//     return window.getComputedStyle(x).backgroundColor;
+// }
+
+
+// function magic(color,x)
+// {
+//     return color.addEventListener("mouseenter",()=>{
+//            center.style.background=x;
+//     });
+// }
+
+
+
+
+// // var x=getcolor(pink);
+// magic(red,getcolor(cyan));
+// magic(blue,getcolor(yellow));
+// magic(yellow,getcolor(red));
+// magic(cyan,getcolor(blue));
